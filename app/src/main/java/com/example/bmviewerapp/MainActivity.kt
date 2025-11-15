a06fbd37df48e25db00b4a0bc7062d86d63a5296
meeten <153901120+meeten@users.noreply.github.com>
2025-11-15 13:56:05 +0200

    feat: implement adding photos from gallery

--- app/src/main/java/com/example/bmviewerapp/MainActivity.kt
+++ app/src/main/java/com/example/bmviewerapp/MainActivity.kt
@@ -1,23 +1,59 @@
 package com.example.bmviewerapp
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.bmviewerapp.ui.theme.BMViewerAppTheme
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            ImageContent()
+        }
+    }
+}
 
+@Composable
+fun ImageContent() {
+    var imageUri by remember { mutableStateOf(Uri.EMPTY) }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = { imageUri = it })
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        AsyncImage(
+            model = imageUri,
+            contentDescription = null,
+            modifier = Modifier.weight(1f)
+        )
+
+        Button(
+            onClick = { launcher.launch(input = "image/*") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+        ) {
+            Text(text = "Get Image")
         }
     }
 }
