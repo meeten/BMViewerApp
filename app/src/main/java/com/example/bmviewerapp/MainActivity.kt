4dc608407c6ca3956ff03e230ae779cf22d98dd2
meeten <153901120+meeten@users.noreply.github.com>
2025-11-15 15:49:35 +0200

    feat: add uri parser to bitmap

--- app/src/main/java/com/example/bmviewerapp/MainActivity.kt
+++ app/src/main/java/com/example/bmviewerapp/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.bmviewerapp
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,14 +23,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ImageContent()
+
+            // Кнопка "Get Image" -> ✅
+            // Обработчик выбора фотографии из галереи -> ✅
+            // Парсим ТОЛЬКО 24bpp
+            // Показываем в Canvas
         }
     }
 }
@@ -38,11 +50,16 @@ fun ImageContent() {
         contract = ActivityResultContracts.GetContent(),
         onResult = { imageUri = it })
 
+    val bitmap = parseBmpFromUri(LocalContext.current, imageUri)
+
+
+    val imageBitmap = bitmap?.asImageBitmap() ?: ImageBitmap(width = 1, height = 1)
+
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        AsyncImage(
-            model = imageUri,
+        Image(
+            bitmap = imageBitmap,
             contentDescription = null,
             modifier = Modifier.weight(1f)
         )
@@ -55,5 +72,16 @@ fun ImageContent() {
         ) {
             Text(text = "Get Image")
         }
+    }
+}
+
+private fun parseBmpFromUri(context: Context, uri: Uri): Bitmap? {
+    return try {
+        context.contentResolver.openInputStream(uri)?.use { inputStream ->
+            BitmapFactory.decodeStream(inputStream)
+        }
+    } catch (e: Exception) {
+        Log.d("URI_TO_BMP", e.message.toString())
+        null
     }
 }
