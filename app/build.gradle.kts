dcafb48c612aa59267c4d84435f8cf6bd191c7ef
meeten <153901120+meeten@users.noreply.github.com>
2025-11-15 13:25:51 +0200

    build: add coil dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // coil
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 }
