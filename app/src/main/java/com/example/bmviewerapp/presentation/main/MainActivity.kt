a6a7d101da540770b0096324999de842f5d8e834
meeten <153901120+meeten@users.noreply.github.com>
2025-11-19 21:36:11 +0200

    feat: recolor status bar to active toolbar color

--- app/src/main/java/com/example/bmviewerapp/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/bmviewerapp/presentation/main/MainActivity.kt
@@ -3,11 +3,18 @@ package com.example.bmviewerapp.presentation.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        enableEdgeToEdge()
         setContent {
+
             MainScreen()
         }
     }
