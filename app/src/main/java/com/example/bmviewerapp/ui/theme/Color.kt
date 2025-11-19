6bed883eda5db095cab40503e5fc1681cd21cdd7
meeten <153901120+meeten@users.noreply.github.com>
2025-11-19 12:38:36 +0200

    build: add NavyBlue color

--- app/src/main/java/com/example/bmviewerapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/bmviewerapp/ui/theme/Color.kt
@@ -9,4 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
 val LightBlue = Color(0xFF1B7EE3)
+val NavyBlue = Color(0xFF0D47A1)
