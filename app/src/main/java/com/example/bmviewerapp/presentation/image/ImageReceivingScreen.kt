5192af9863dcbb3ee846f2baba3793410470d100
meeten <153901120+meeten@users.noreply.github.com>
2025-11-19 12:41:35 +0200

    refactor: Move Scaffold from MainScreen to individual screens

--- app/src/main/java/com/example/bmviewerapp/presentation/image/ImageReceivingScreen.kt
+++ app/src/main/java/com/example/bmviewerapp/presentation/image/ImageReceivingScreen.kt
@@ -12,7 +12,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -27,8 +32,9 @@ import androidx.compose.ui.unit.sp
 import com.example.bmviewerapp.R
 import com.example.bmviewerapp.ui.theme.LightBlue
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ImageReceivingScreen(modifier: Modifier, onClickGetImageButton: (Uri) -> Unit) {
+fun ImageReceivingScreen(onClickGetImageButton: (Uri) -> Unit) {
     var imageUri by remember { mutableStateOf(Uri.EMPTY) }
 
     if (imageUri != Uri.EMPTY) {
@@ -39,45 +45,62 @@ fun ImageReceivingScreen(modifier: Modifier, onClickGetImageButton: (Uri) -> Uni
         contract = ActivityResultContracts.GetContent(),
         onResult = { imageUri = it })
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(R.drawable.hide_image),
-            contentDescription = null
-        )
-
-        Spacer(modifier = Modifier.height(5.dp))
-
-        Text(
-            text = "Select a photo",
-            fontSize = 25.sp,
-            fontWeight = FontWeight.Bold
-        )
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Text(
-            text = "Choose a photo to edit",
-            fontSize = 15.sp
-        )
-
-        Spacer(modifier = Modifier.height(60.dp))
-
-        Button(
-            onClick = {
-                launcher.launch(input = "image/*")
-            },
-            modifier = Modifier,
-            colors = ButtonDefaults.buttonColors(containerColor = LightBlue)
-        ) {
-            Text(
-                text = "GET IMAGE",
-                modifier = Modifier.padding(vertical = 5.dp, horizontal = 45.dp),
-                fontSize = 20.sp,
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Photo Editor",
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = LightBlue)
             )
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.hide_image),
+                contentDescription = null
+            )
+
+            Spacer(modifier = Modifier.height(5.dp))
+
+            Text(
+                text = "Select a photo",
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Text(
+                text = "Choose a photo to edit",
+                fontSize = 15.sp
+            )
+
+            Spacer(modifier = Modifier.height(60.dp))
+
+            Button(
+                onClick = {
+                    launcher.launch(input = "image/*")
+                },
+                modifier = Modifier,
+                colors = ButtonDefaults.buttonColors(containerColor = LightBlue)
+            ) {
+                Text(
+                    text = "GET IMAGE",
+                    modifier = Modifier.padding(vertical = 5.dp, horizontal = 45.dp),
+                    fontSize = 20.sp,
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/example/bmviewerapp/presentation/main/MainScreen.kt
+++ app/src/main/java/com/example/bmviewerapp/presentation/main/MainScreen.kt
@@ -1,50 +1,26 @@
 package com.example.bmviewerapp.presentation.main
 
-import android.util.Log
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import com.example.bmviewerapp.navigation.AppNavGraph
 import com.example.bmviewerapp.navigation.rememberNavigationState
 import com.example.bmviewerapp.presentation.image.ImageEditorScreen
 import com.example.bmviewerapp.presentation.image.ImageReceivingScreen
-import com.example.bmviewerapp.ui.theme.LightBlue
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
     val navigationState = rememberNavigationState()
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = "Photo Editor",
-                        color = Color.White,
-                        fontWeight = FontWeight.Bold
-                    )
-                },
-                colors = TopAppBarDefaults.topAppBarColors(containerColor = LightBlue)
-            )
-        }) {
-        AppNavGraph(
-            navHostController = navigationState.navHostController,
-            imageReceivingScreenContent = {
-                ImageReceivingScreen(modifier = Modifier.padding(it)) { imageUri ->
-                    navigationState.navigateToImageEditScreen(imageUri)
-                }
-            },
-            imageEditorScreenContent = { imageUri ->
-                ImageEditorScreen(imageUri)
+    AppNavGraph(
+        navHostController = navigationState.navHostController,
+        imageReceivingScreenContent = {
+            ImageReceivingScreen { imageUri ->
+                navigationState.navigateToImageEditScreen(imageUri)
             }
-        )
-    }
+        },
+        imageEditorScreenContent = { imageUri ->
+            ImageEditorScreen(imageUri) {
+                navigationState.navHostController.popBackStack()
+            }
+        }
+    )
 }
